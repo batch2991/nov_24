dc8126fb4bef37601077eee8a120376f00253519
venkat <venkatgn.gutta@gmail.com>
2023-02-24 19:43:57 +0530

    order class updated with methods

--- Ebay/src/main/java/pages/Orders.java
+++ Ebay/src/main/java/pages/Orders.java
@@ -4,7 +4,11 @@ public class Orders
 {
 public void addtocart()
 {
-
+   System.out.println("code to place a new order");
+}
+public void createorder()
+{
+	System.out.println("code to new order");
 }
 
 
